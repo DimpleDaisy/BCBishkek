852441417a4780569cc99a8cca6f9207d609c092
Your Name <you@example.com>
2024-01-07 12:14:37 -0800

    Add Template Files

--- store/views.py
+++ store/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
-
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
@@ -16,22 +15,20 @@ def about(request):
 
 def login_user(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, ("You Have Been Logged In!"))
-            return redirect('home')
-        else:
-            messages.success(request, ("There was an error, please try again"))
-            return redirect('Login')
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request,("You Have Been Logged In!"))
+                return redirect('home')
+            else:
+                 messages.success(request,("There was an error, please try again"))
+                 return redirect('Login')
     else:
-        return render(request, 'login.html', {})
-
-
+            return render(request, 'login.html', {})
 
 def logout_user(request):
     logout(request)
-    messages.success(request, ("You have been logged out"))
+    messages.success(request,("You have been logged out"))
     return redirect('home')
