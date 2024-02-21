caadf1431077ef0187baf5636efb2a067bd4367a
Your Name <you@example.com>
2024-02-21 03:42:56 -0800

    add some new functions

--- store/urls.py
+++ store/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('password/', auth_views.PasswordResetView.as_view(template_name='registration/change-password.html')),
+    path('product/<int:pk>', views.product, name='product'),
+    path('category/<str:foo>', views.category, name='category'),
+
 ]
